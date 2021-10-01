537ae2eecfc9a96a726ba86536632b1131840506
Jaroslav-Melichar <jarabaltazar@seznam.cz>
2021-10-01 13:44:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<?php 
-$computer
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,3 +6,17 @@ $computer
     <title>Prvni pole</title>
 </head>
 <body>
+    <h1> <b>Můj počítač</b> </h1>
+
+<?php
+$computer = array(
+'CPU' => "AMD Ryzen 7 3700x",
+'RAM' => "2X HyperX Predator 8GB DDR4 3200",
+'SDD' => "Samsung SSD 860 Pro, 2,5 - 1TB",
+'CASE' => "Thermaltake View 21 TG RGB PLUS",
+'MOUSE' => "A4Tech Bloody V7 Ultra Core 3",
+'HEADPHONES' => "HyperX Cloud II Wireless",
+'DISPLAY' => "Dell S2721HGF - LED monitor 27",
+'MAINBOARD' =>"GIGABYTE B450M DS3H - AMD B450",
+);
+?>
